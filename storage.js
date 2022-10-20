4dfb3ac66cfa6621b690ceacc2bfc76e331c5731
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-20 11:32:45 +0600

    first commit

---
+++ storage.js
@@ -0,0 +1,15 @@
+const addToLocalStorage = () => {
+    const idInput = document.getElementById('storage-id');
+    const id = idInput.value;
+    const idValue = document.getElementById('storage-value');
+    const value = idValue.value;
+
+    if (id && value) {
+        localStorage.setItem(id, value);
+    }
+    idInput.value = '';
+    idValue.value = '';
+    localStorage.setItem('')
+}
+
+
